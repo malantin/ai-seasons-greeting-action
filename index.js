0dcf51dca2e15543275839c16cc2d7b49244e420
Malte Lantin <malantin@github.com>
2023-11-29 21:22:50 +0000

    Refactor action code

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const httpm = require('@actions/http-client');
 
-async function makePostRequest(prompt, apikey) {
+async function makePostRequest() {
+    const prompt = core.getInput('prompt');
+    const apikey = core.getInput('api-key');
+    const endpoint = core.getInput('endpoint');
+
     let http = new httpm.HttpClient('http-client');
     let data = {
         messages: [
-            {role: 'system', content: 'You generate short Christmas greetings. The message should be 160 characters or less to fit into an SMS. The user will tell you who the message is for and will provide more context if needed. You will only answer with the text for the sms, nothing else.'},
-            {role: 'user', content: 'Mother Haven\'t seen her for a long time✨'},
-            {role: 'assistant', content: 'Merry Christmas Mum! I miss you! 🎄'},
-            {role: 'user', content: 'Laura is a colleague of mine'},
-            {role: 'assistant', content: 'Have a great Holiday Season Laura! Thanks for the great partnership this year. '},
-            {role: 'user', content: prompt}
+            { role: 'system', content: 'You generate short Christmas greetings. The message should be 160 characters or less to fit into an SMS. The user will tell you who the message is for and will provide more context if needed. You will only answer with the text for the sms, nothing else.' },
+            { role: 'user', content: 'Mother Haven\'t seen her for a long time✨' },
+            { role: 'assistant', content: 'Merry Christmas Mum! I miss you! 🎄' },
+            { role: 'user', content: 'Laura is a colleague of mine' },
+            { role: 'assistant', content: 'Have a great Holiday Season Laura! Thanks for the great partnership this year. ' },
+            { role: 'user', content: prompt }
         ],
         max_tokens: 800,
         temperature: 0.7,
@@ -24,16 +28,13 @@ async function makePostRequest(prompt, apikey) {
         'Content-Type': 'application/json',
         'api-key': apikey
     };
-    const endpoint = core.getInput('endpoint');
+
     let res = await http.postJson(endpoint, data, headers);
     return res.result;
 }
 
 try {
-    const prompt = core.getInput('prompt');
-    const apikey = core.getInput('api-key');
-    
-    makePostRequest(prompt,apikey).then(response => {
+    makePostRequest().then(response => {
         let message = response.choices[0].message.content;
         console.log(message);
         core.setOutput("seasons-greeting", message);
